6a8ab8515e0d7c73ac91db80124db1967dfbf431
Mauricio Voto <mauriciovoto@gmail.com>
2021-01-12 10:32:54 -0800

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExThinkificAPI.Mixfile do
 
   def project do
     [app: :ex_thinkific_api,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
